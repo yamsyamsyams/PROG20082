4ba7b5f813a45b2329b9a9c95d050a914f4b80d5
James Hoang <j.hoang2080@gmail.com>
2020-09-17 10:46:39 -0400

    updated - added functionality to create account - added night/day mode

--- PayPark/app/src/main/java/com/example/paypark/SignInActivity.kt
+++ PayPark/app/src/main/java/com/example/paypark/SignInActivity.kt
@@ -1,11 +1,31 @@
 package com.example.paypark
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var tvCreateAccount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        tvCreateAccount = findViewById(R.id.tvCreateAccount)
+        tvCreateAccount.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?){
+        if(v?.id == tvCreateAccount.id){ // v? if v != null
+            // go to SignUpActivity
+            this.goToSignUp()
+        }
+    }
+
+    private fun goToSignUp(){
+        val signUpIntent = Intent(this, SignUpActivity::class.java)
+        startActivity(signUpIntent)
     }
 }
