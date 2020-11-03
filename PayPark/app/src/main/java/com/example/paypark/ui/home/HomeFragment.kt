e9ca4d7b50ffb6c49498d852fca9867c55cf2379
James Hoang <j.hoang2080@gmail.com>
2020-11-03 15:32:35 -0500

    edited startup project

--- PayPark/app/src/main/java/com/example/paypark/ui/home/HomeFragment.kt
+++ PayPark/app/src/main/java/com/example/paypark/ui/home/HomeFragment.kt
@@ -8,7 +8,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.paypark.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment() {
 
@@ -21,8 +23,10 @@ class HomeFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
-
-
+        val fabAddParking: FloatingActionButton = root.findViewById(R.id.fabAddParking)
+        fabAddParking.setOnClickListener{
+            findNavController().navigate(R.id.action_nav_home_to_add_parking_fragment)
+        }
 
         return root
     }
